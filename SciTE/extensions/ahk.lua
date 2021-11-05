e1713be7a2a8cd823e0f8513d0cd7557e7f8fba0
telppa <telppa@users.noreply.github.com>
2021-11-05 21:08:50 +0800

    将 lua 插件移动到 “SciTE\extensions”

--- SciTE/extensions/ahk.lua
+++ SciTE/extensions/ahk.lua
@@ -667,7 +667,7 @@ if FileExists(extlua) then
 end
 
 -- SciTE4AutoHotkey-Plus 的增强
--- dofile(props['SciteDefaultHome'].."/tools/运行选区代码.lua")
-dofile(props['SciteDefaultHome'].."/tools/自动完成增强版.lua")
-dofile(props['SciteDefaultHome'].."/tools/新建文件时默认UTF-8带BOM.lua")
+-- dofile(props['SciteDefaultHome'].."/extensions/运行选区代码.lua")
+dofile(props['SciteDefaultHome'].."/extensions/自动完成增强版.lua")
+dofile(props['SciteDefaultHome'].."/extensions/新建文件时默认UTF-8带BOM.lua")
 -- dofile(props['SciteUserHome']..'/lexers/lpeg_s4a.lua')
